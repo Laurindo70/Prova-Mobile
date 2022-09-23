6b18b397eb434f7483e4b007eaffa05a2f39bb9f
Ronan Laurindo Flor <ronanlf12@gmail.com>
2022-09-23 19:29:18 -0400

    conexão e criação com banco concluida

---
+++ app/src/main/java/com/example/provap1/banco/DBHelper.java
@@ -0,0 +1,33 @@
+package com.example.provap1.banco;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public DBHelper(@Nullable Context context){
+        super(context, "BancoListaTelefonica", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL(
+                "create table abastecimentos(" +
+                        "id integer primary key autoincrement," +
+                        "quilometragem varchar(20)," +
+                        "quantidade_abastecida(10)," +
+                        "data_abastecimento(15)," +
+                        "valor(20));"
+        );
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
