1cb1bcbf21e4f613b0cb7d5d4916a691aac90493
Ronan Laurindo Flor <ronanlf12@gmail.com>
2022-09-23 19:39:14 -0400

    mapeamento elementos da tela

--- app/src/main/java/com/example/provap1/MainActivity.java
+++ app/src/main/java/com/example/provap1/MainActivity.java
@@ -3,12 +3,47 @@ package com.example.provap1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.provap1.banco.DBHelper;
+import com.example.provap1.model.Abastecimento;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText quilometragem;
+    private EditText quantidadeAbastecida;
+    private EditText dataAbastecimento;
+    private EditText valor;
+    private EditText media;
+    List<Abastecimento> dados;
+    ListView listagem;
+    DBHelper db;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new DBHelper(this);
+
+        quilometragem = findViewById(R.id.quilometragem);
+        quantidadeAbastecida = findViewById(R.id.quantidadeAbastecida);
+        dataAbastecimento = findViewById(R.id.diaAbastecimento);
+        valor = findViewById(R.id.valor);
+        media = findViewById(R.id.mediaConsulmo);
+
+        dados = new ArrayList();
+
+        ArrayAdapter adapter =
+                new ArrayAdapter(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, dados);
+        listagem.setAdapter(adapter);
+
     }
 }
