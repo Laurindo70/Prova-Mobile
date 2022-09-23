3bbb92873808ad1185b8243d141c9470ec143fd6
Ronan Laurindo Flor <ronanlf12@gmail.com>
2022-09-23 19:31:48 -0400

    Criação do model responsavel pela capitação dos dados.

---
+++ app/src/main/java/com/example/provap1/model/Abastecimento.java
@@ -0,0 +1,50 @@
+package com.example.provap1.model;
+
+public class Abastecimento {
+
+    private int id;
+    private String quilometragem;
+    private String quatidadeAbastecida;
+    private String data_abastecimento;
+    private String valor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(String quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public String getQuatidadeAbastecida() {
+        return quatidadeAbastecida;
+    }
+
+    public void setQuatidadeAbastecida(String quatidadeAbastecida) {
+        this.quatidadeAbastecida = quatidadeAbastecida;
+    }
+
+    public String getData_abastecimento() {
+        return data_abastecimento;
+    }
+
+    public void setData_abastecimento(String data_abastecimento) {
+        this.data_abastecimento = data_abastecimento;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
